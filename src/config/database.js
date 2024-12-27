37cf7360a9054893d32c0b8febfb30b653badbed
Abhishek Kumar Suraj <32790427+abhishekkumarsuraj@users.noreply.github.com>
2024-12-27 08:30:03 +0530

    Update database.js
    removed password

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://devtinder:jVo6xyMDgY9vS0OP@devtinder.4j0cc.mongodb.net/devtinder"
+    "mongodb+srv://devtinder:xxxxxxxxxxx@devtinder.4j0cc.mongodb.net/devtinder"
   );
 };
 
